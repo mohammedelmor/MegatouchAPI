87eaafb4bd1143fb85d76e3170891631853f1539
mohammedelmor <mohammedelmor7@gmail.com>
2024-03-04 13:28:22 +0400

    Adding InflatableRepository and InflatableCategoryRepository

---
+++ src/main/java/org/megatouch/api/repository/InflatableCategoryRepository.java
@@ -0,0 +1,7 @@
+package org.megatouch.api.repository;
+
+import org.megatouch.api.entity.InflatableCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InflatableCategoryRepository extends JpaRepository<InflatableCategory, Long> {
+}
---
+++ src/main/java/org/megatouch/api/repository/InflatableRepository.java
@@ -0,0 +1,7 @@
+package org.megatouch.api.repository;
+
+import org.megatouch.api.entity.Inflatable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InflatableRepository extends JpaRepository<Inflatable, Long> {
+}
