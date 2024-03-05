a9181d0365ae871029fce6124d208a8076367212
mohammedelmor <mohammedelmor7@gmail.com>
2024-03-05 09:45:10 +0400

    Fixing oauth2 resource server and keycloak integration

--- src/main/java/org/megatouch/api/security/SecurityConfig.java
+++ src/main/java/org/megatouch/api/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package org.megatouch.api.security;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.megatouch.api.config.KeycloakConfigProps;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,15 +14,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final KeycloakConfigProps keycloakConfigProps;
+
+    public SecurityConfig(KeycloakConfigProps keycloakConfigProps) {
+        this.keycloakConfigProps = keycloakConfigProps;
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests ->
                 authorizeRequests.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
         ).oauth2ResourceServer((oauth2) -> {
-           oauth2.jwt(Customizer.withDefaults());
+           oauth2.jwt(jwt -> jwt.jwkSetUri(keycloakConfigProps.getKeySet()));
         });
         return http.build();
+    }
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
 }
